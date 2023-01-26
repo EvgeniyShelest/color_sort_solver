8bc7e1bb230c41726fd2bba955d52f637419479b
Evgeniy Shelest <zhck12@gmail.com>
2023-01-26 18:52:59 +0300

    Potemtial moves

--- flask_set.rb
+++ flask_set.rb
@@ -42,13 +42,46 @@ class FlaskSet
   end
 
   def solved?
-    @flask_set.all? { |flask| monofilled_or_empty?(flask) }
+    @flask_set.all? { |flask| monofilled?(flask) || empty?(flask) }
   end
 
 private
 
-  def monofilled_or_empty?(flask)
-    flask[1..].all? { |el| el === flask[0] }
+  def potential_moves
+    return @potential_moves if defined?(@potential_moves)
+
+    @potential_moves = []
+    @flask_set.each_with_index do |flask_from, flask_from_index|
+      next if partially_monofilled?(flask_from) || monofilled?(flask_from) || empty?(flask_from)
+      last_non_empty_index_from = last_non_empty_index(flask_from)
+      from_index = [flask_from_index, last_non_empty_index_from]
+      el = flask_from[last_non_empty_index_from]
+
+      @flask_set.each_with_index do |flask_to, flask_to_index|
+        next if flask_to_index == flask_from_index
+        next if monofilled?(flask_to)
+        if empty?(flask_to)
+          @potential_moves << [from_index, [flask_to_index, 0]]
+          next
+        end
+        last_non_empty_index_to = last_non_empty_index(flask_to)
+        next if flask_to[last_non_empty_index_to] != el
+        @potential_moves << [from_index, [flask_to_index, last_non_empty_index_to+1]]
+      end
+    end
+    @potential_moves
+  end
+
+  def monofilled?(flask)
+    flask[1..].all? { |el| el === flask[0] && !el.zero? }
+  end
+
+  def partially_monofilled?(flask)
+    flask[1...-1].all? { |el| el === flask[0] && !el.zero? } && flask[-1].zero?
+  end
+
+  def empty?(flask)
+    flask.all? { |el| el.zero? }
   end
 
   def last_non_empty_index(flask)
